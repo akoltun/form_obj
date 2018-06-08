4d2cc4cfcbcecff774ec483e7dd5b13fe3efd9ee
Alexander Koltun <alexander.koltun@vasco.com>
2018-06-08 19:43:37 +0200

    Refactor struct update_attributes

--- lib/form_obj/struct/array.rb
+++ lib/form_obj/struct/array.rb
@@ -3,9 +3,13 @@ require 'typed_array'
 module FormObj
   class Struct
     class Array < TypedArray
-      def create(hash = nil, raise_if_not_found: true)
-        self << (item = create_item(hash, raise_if_not_found: raise_if_not_found))
+      def build(hash = nil, raise_if_not_found: true)
+        self << (item = build_item(hash, raise_if_not_found: raise_if_not_found))
         item
+      end
+
+      def create(*args)
+        build(*args)
       end
 
       def to_hash
@@ -13,41 +17,62 @@ module FormObj
       end
 
       def update_attributes(vals, raise_if_not_found:)
-        ids_exists = []
         items_to_add = []
+        items_to_update = {}
 
         vals.each do |val|
-          id = primary_key(HashWithIndifferentAccess.new(val))
-          item = self.find { |i| i.primary_key == id }
+          val = HashWithIndifferentAccess.new(val)
+          item = find_by_primary_key(primary_key(val))
           if item
-            item.update_attributes(val, raise_if_not_found: raise_if_not_found)
-            ids_exists << id
+            items_to_update[item] = val
           else
             items_to_add << val
           end
         end
+        items_to_destroy = items_for_destruction(items_to_add: items_to_add, items_to_update: items_to_update)
 
-        delete_items(self.map(&:primary_key) - ids_exists)
-
-        items_to_add.each do |item|
-          self << create_item(item, raise_if_not_found: raise_if_not_found)
-        end
+        destroy_items(items_to_destroy)
+        update_items(items_to_update, raise_if_not_found: raise_if_not_found)
+        build_items(items_to_add, raise_if_not_found: raise_if_not_found)
 
         sort! { |a, b| (vals.index { |val| primary_key(val) == a.primary_key } || -1) <=> (vals.index { |val| primary_key(val) == b.primary_key } || -1) }
       end
 
       private
 
-      def delete_items(ids)
-        self.delete_if { |item| ids.include? item.primary_key }
+      def primary_key(hash)
+        hash[item_class.primary_key]
       end
 
-      def create_item(hash, raise_if_not_found:)
+      def find_by_primary_key(id)
+        find { |item| item.primary_key == id }
+      end
+
+      def build_item(hash, raise_if_not_found:)
         item_class.new(hash, raise_if_not_found: raise_if_not_found)
       end
 
-      def primary_key(hash)
-        hash[item_class.primary_key]
+      # items_to_add - array of hashes of new attribute values
+      # items_to_update - hash where key is the item to update and value is a hash of new attribute values
+      def items_for_destruction(items_to_add:, items_to_update:)
+        self - items_to_update.keys
+      end
+
+      # items - array of items to be destroyed
+      def destroy_items(items)
+        items.each { |item| delete(item) }
+      end
+
+      # items - hash where key is the item to update and value is a hash of new attribute values
+      # params - additional params for :update_attributes method
+      def update_items(items, params)
+        items.each_pair { |item, attr_values| item.update_attributes(attr_values, params) }
+      end
+
+      # items - array of hashes of new attribute values
+      # params - additional params for constructor
+      def build_items(items, params)
+        items.each { |item| self << build_item(item, params) }
       end
     end
   end
