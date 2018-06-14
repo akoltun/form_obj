b2998cf5e46a016e78a7a3cd106a21a36d30d8b8
Alexander Koltun <alexander.koltun@gmail.com>
2018-06-14 12:21:03 +0200

    Add minitests for FormObj::ModelMapper inherited from FormObj::Form.

---
+++ test/model_mapper/inherited_from_form/inherited_from_struct/assign_values_test.rb
@@ -0,0 +1,125 @@
+require "test_helper"
+
+class ModelMapperAssignValuesTest < Minitest::Test
+  class Suspension < FormObj::Form
+    include FormObj::ModelMapper
+
+    attribute :front
+    attribute :rear
+  end
+  class Chassis < FormObj::Form
+    include FormObj::ModelMapper
+
+    attribute :suspension, class: Suspension
+    attribute :brakes
+  end
+  class Colour < FormObj::Form
+    include FormObj::ModelMapper
+
+    attribute :name
+    attribute :rgb
+  end
+  class Team < FormObj::Form
+    include FormObj::ModelMapper
+
+    attribute :name
+    attribute :year
+    attribute :cars, array: true do
+      attribute :code
+      attribute :driver
+      attribute :engine do
+        attribute :power
+        attribute :volume
+      end
+      attribute :chassis, class: 'ModelMapperAssignValuesTest::Chassis'
+    end
+    attribute :colours, class: Colour, array: true
+  end
+
+  def setup
+    @team = Team.new
+
+    @team.name = 'Ferrari'
+    @team.year = 1950
+
+    car = @team.cars.create
+    car.code = '340 F1'
+    car.driver = 'Ascari'
+    car.engine.power = 335
+    car.engine.volume = 4.1
+    car.chassis.suspension.front = 'independent'
+    car.chassis.suspension.rear = 'de Dion'
+    car.chassis.brakes = :drum
+
+    suspension = Suspension.new
+    suspension.front = 'dependent'
+    suspension.rear = 'de Lion'
+
+    chassis = Chassis.new
+    chassis.suspension = suspension
+    chassis.brakes = :disc
+
+    car = @team.cars.create
+    car.code = '275 F1'
+    car.driver = 'Villoresi'
+    car.engine.power = 300
+    car.engine.volume = 3.3
+    car.chassis = chassis
+
+    colour = @team.colours.create
+    colour.name = :red
+    colour.rgb = 0xFF0000
+    
+    colour = Colour.new
+    colour.name = :white
+    colour.rgb = 0xFFFFFF
+    @team.colours << colour
+  end
+
+  def test_that_all_values_are_correctly_assigned
+    assert_equal('Ferrari', @team.name)
+    assert_equal(1950,      @team.year)
+
+    assert_equal(2, @team.cars.size)
+
+    assert_equal('340 F1',      @team.cars[0].code)
+    assert_equal('Ascari',      @team.cars[0].driver)
+    assert_equal(335,           @team.cars[0].engine.power)
+    assert_equal(4.1,           @team.cars[0].engine.volume)
+    assert_equal('independent', @team.cars[0].chassis.suspension.front)
+    assert_equal('de Dion',     @team.cars[0].chassis.suspension.rear)
+    assert_equal(:drum,         @team.cars[0].chassis.brakes)
+
+    assert_equal('275 F1',    @team.cars[1].code)
+    assert_equal('Villoresi', @team.cars[1].driver)
+    assert_equal(300,         @team.cars[1].engine.power)
+    assert_equal(3.3,         @team.cars[1].engine.volume)
+    assert_equal('dependent', @team.cars[1].chassis.suspension.front)
+    assert_equal('de Lion',   @team.cars[1].chassis.suspension.rear)
+    assert_equal(:disc,       @team.cars[1].chassis.brakes)
+
+    assert_equal(2, @team.colours.size)
+    
+    assert_equal(:red,      @team.colours[0].name)
+    assert_equal(0xFF0000,  @team.colours[0].rgb)
+
+    assert_equal(:white,    @team.colours[1].name)
+    assert_equal(0xFFFFFF,  @team.colours[1].rgb)
+  end
+
+  def test_that_non_existent_attribute_raises_error
+    assert_raises(NoMethodError) { @team.non_existent_attribute }
+
+    assert_raises(NoMethodError) { @team.cars[0].non_existent_attribute }
+    assert_raises(NoMethodError) { @team.cars[0].engine.non_existent_attribute }
+
+    assert_raises(NoMethodError) { @team.cars[1].non_existent_attribute }
+    assert_raises(NoMethodError) { @team.cars[1].engine.non_existent_attribute }
+
+    assert_raises(NoMethodError) { @team.colours[0].non_existent_attribute }
+    assert_raises(NoMethodError) { @team.colours[0].engine.non_existent_attribute }
+
+    assert_raises(NoMethodError) { @team.colours[1].non_existent_attribute }
+    assert_raises(NoMethodError) { @team.colours[1].engine.non_existent_attribute }
+  end
+end
---
+++ test/model_mapper/inherited_from_form/inherited_from_struct/duplicate_attribute_test.rb
@@ -0,0 +1,111 @@
+require "test_helper"
+
+class ModelMapperDuplicateAttributeTest < Minitest::Test
+  class Suspension < FormObj::Form
+    include FormObj::ModelMapper
+
+    attribute :front, default: 'dependant'
+    attribute :front
+
+    attribute :fantastic, default: :flying
+    attribute :fantastic, default: :jumping
+
+    attribute :rear
+    attribute :rear, default: 'de Dion'
+  end
+
+  class OldChassis < FormObj::Form
+    include FormObj::ModelMapper
+
+    attribute :old_suspension, class: Suspension
+    attribute :old_brakes
+  end
+
+  class Chassis < FormObj::Form
+    include FormObj::ModelMapper
+
+    attribute :suspension, class: Suspension
+    attribute :brakes
+  end
+
+  class Colour < FormObj::Form
+    include FormObj::ModelMapper
+
+    attribute :name
+    attribute :rgb
+  end
+
+  class Team < FormObj::Form
+    include FormObj::ModelMapper
+
+    attribute :full_name, default: 'Scuderia Ferrari'
+    attribute :full_name
+
+    attribute :name, default: 'Ferrari'
+    attribute :name, default: 'McLaren'
+
+    attribute :year
+    attribute :year, default: 1950
+
+    attribute :cars, array: true do
+      attribute :wheel
+    end
+    attribute :cars, array: true do
+      attribute :code
+      attribute :driver
+
+      attribute :engine do
+        attribute :old_power
+        attribute :volume, default: 5.0
+      end
+      attribute :engine do
+        attribute :power
+        attribute :volume
+      end
+
+      attribute :chassis, class: 'ModelMapperDuplicateAttributeTest::OldChassis'
+      attribute :chassis, class: 'ModelMapperDuplicateAttributeTest::Chassis'
+    end
+
+    attribute :colours, class: Colour, array: true
+    attribute :colours
+  end
+
+  def test_that_team_has_only_one_attribute_of_each_name
+    assert_equal(%i{full_name name year cars colours}, Team.attributes)
+  end
+
+  def test_that_team_attributes_apply_only_last_parameters
+    team = Team.new
+
+    assert_nil(team.full_name)
+    assert_equal('McLaren', team.name)
+    assert_equal(1950, team.year)
+    assert_nil(team.colours)
+  end
+
+  def test_that_car_has_only_one_attribute_of_each_name
+    assert_equal(%i{code driver engine chassis}, Team.new.cars.create.class.attributes)
+  end
+
+  def test_that_car_attributes_apply_only_last_parameters
+    car = Team.new.cars.create
+
+    assert_equal(%i{power volume}, car.engine.class.attributes)
+    assert_nil(car.engine.volume)
+
+    assert_kind_of(Chassis, car.chassis)
+  end
+
+  def test_that_suspension_has_only_one_attribute_of_each_name
+    assert_equal(%i{front fantastic rear}, Team.new.cars.create.chassis.suspension.class.attributes)
+  end
+
+  def test_that_suspension_attributes_apply_only_last_parameters
+    suspension = Team.new.cars.create.chassis.suspension
+
+    assert_nil(suspension.front)
+    assert_equal(:jumping, suspension.fantastic)
+    assert_equal('de Dion', suspension.rear)
+  end
+end
