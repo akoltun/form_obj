ebe3456b97ab00cda2fcf5c9d3bc68c72ede3d8e
Alexander Koltun <alexander.koltun@gmail.com>
2018-07-03 14:43:56 +0200

    Add test case to load_from_models when form is already prefilled

--- test/model_mapper/load_from_models_test.rb
+++ test/model_mapper/load_from_models_test.rb
@@ -91,28 +91,107 @@ class ModelMapperLoadFromModelsTest < Minitest::Test
     ]
   end
 
+  def fill_in_form
+    @team.name = 'McLaren'
+    @team.year = 1966
+
+    car = @team.cars.create
+    car.code = '340 F1'
+    car.driver = 'Bruce McLaren'
+    car.engine.power = 300
+    car.engine.volume = 3.0
+
+    car = @team.cars.create
+    car.code = 'M7A'
+    car.driver = 'Denis Hulme'
+    car.engine.power = 415
+    car.engine.volume = 4.3
+
+    sponsor = @team.sponsors.create
+    sponsor.title = 'Total'
+    sponsor.money = 250
+
+    sponsor = @team.sponsors.create
+    sponsor.title = 'Shell'
+    sponsor.money = 3000
+
+    chassis = @team.chassis.create
+    chassis.suspension.front = 'old'
+    chassis.suspension.rear = 'very old'
+    chassis.brakes = :hand
+
+    colour = @team.colours.create
+    colour.name = 'white'
+    colour.rgb = 0xFFFFFF
+
+    drivers_championship = @team.drivers_championships.create
+    drivers_championship.driver = 'Ascari'
+    drivers_championship.year = 1952
+
+    drivers_championship = @team.drivers_championships.create
+    drivers_championship.driver = 'Hawthorn'
+    drivers_championship.year = 1958
+
+    constructors_championship = @team.constructors_championships.create
+    constructors_championship.year = 1961
+
+    constructors_championship = @team.constructors_championships.create
+    constructors_championship.year = 1964
+  end
+
   def test_that_load_from_model_returns_form_itself
     assert_same(@team, @team.load_from_model(@team_model))  
   end
 
-  def test_that_all_attributes_value_are_correctly_loaded_from_filled_models_into_empty_form
-    check_that_all_attributes_value_are_correctly_loaded_from_filled_models
+  def test_that_all_attributes_value_are_correctly_loaded_from_empty_models_into_empty_form
+    check_that_all_attributes_value_are_correctly_loaded_from_empty_models
+    check_that_not_synched_attributes_are_still_empty
   end
 
-  def test_that_all_attributes_value_are_correctly_loaded_from_filled_models_into_filled_form
-    2.times { @team.cars.build }
-
-    check_that_all_attributes_value_are_correctly_loaded_from_filled_models
-  end
-
-  def test_that_all_attributes_value_are_correctly_loaded_from_filled_models_into_empty_form
-    check_that_all_attributes_value_are_correctly_loaded_from_filled_models
-  end
-
-  def test_that_all_attributes_value_are_correctly_loaded_from_filled_models_into_filled_form
-    2.times { @team.cars.build }
+  def test_that_all_attributes_value_are_correctly_loaded_from_empty_models_into_filled_form
+    fill_in_form
 
     check_that_all_attributes_value_are_correctly_loaded_from_empty_models
+    check_that_not_synched_attributes_keep_their_values
+  end
+
+  def test_that_all_attributes_value_are_correctly_loaded_from_filled_models_into_empty_form
+    fill_in_models
+
+    check_that_all_attributes_value_are_correctly_loaded_from_filled_models
+    check_that_not_synched_attributes_are_still_empty
+  end
+
+  def test_that_all_attributes_value_are_correctly_loaded_from_filled_models_into_filled_form
+    fill_in_models
+    fill_in_form
+
+    check_that_all_attributes_value_are_correctly_loaded_from_filled_models
+    check_that_not_synched_attributes_keep_their_values
+  end
+
+  def check_that_not_synched_attributes_are_still_empty
+    assert_kind_of(FormObj::ModelMapper::Array, @team.drivers_championships)
+    assert_equal(0, @team.drivers_championships.size)
+
+    assert_kind_of(FormObj::ModelMapper::Array, @team.constructors_championships)
+    assert_equal(0, @team.constructors_championships.size)
+  end
+
+  def check_that_not_synched_attributes_keep_their_values
+    assert_kind_of(FormObj::ModelMapper::Array, @team.drivers_championships)
+    assert_equal(2, @team.drivers_championships.size)
+
+    assert_equal('Ascari', @team.drivers_championships[0].driver)
+    assert_equal(1952, @team.drivers_championships[0].year)
+
+    assert_equal('Hawthorn', @team.drivers_championships[1].driver)
+    assert_equal(1958, @team.drivers_championships[1].year)
+
+    assert_kind_of(FormObj::ModelMapper::Array, @team.constructors_championships)
+    assert_equal(2, @team.constructors_championships.size)
+    assert_equal(1961, @team.constructors_championships[0].year)
+    assert_equal(1964, @team.constructors_championships[1].year)
   end
 
   def check_that_all_attributes_value_are_correctly_loaded_from_empty_models
@@ -135,7 +214,6 @@ class ModelMapperLoadFromModelsTest < Minitest::Test
   end
 
   def check_that_all_attributes_value_are_correctly_loaded_from_filled_models
-    fill_in_models
     @team.load_from_models(default: @team_model, chassis: @chassis_model)
 
     assert_equal(@team_model.team_name, @team.name)
@@ -173,9 +251,6 @@ class ModelMapperLoadFromModelsTest < Minitest::Test
     assert_equal(@chassis_model.chassis[1][:suspension].front, @team.chassis[1].suspension.front)
     assert_equal(@chassis_model.chassis[1][:suspension].rear, @team.chassis[1].suspension.rear)
     assert_equal(@chassis_model.chassis[1][:brakes], @team.chassis[1].brakes)
-
-    assert_equal(Array.new, @team.drivers_championships)
-    assert_equal(Array.new, @team.constructors_championships)
 
     assert_kind_of(FormObj::ModelMapper::Array, @team.colours)
     assert_equal(3, @team.colours.size)
